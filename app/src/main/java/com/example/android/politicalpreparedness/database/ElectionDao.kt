b29f3fe306545111c6377e735ed31994340879a2
diablouma <mh732@hotmail.com>
2021-09-06 16:45:02 +0200

    adds insert and getall methods to dao

--- app/src/main/java/com/example/android/politicalpreparedness/database/ElectionDao.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/database/ElectionDao.kt
@@ -1,5 +1,6 @@
 package com.example.android.politicalpreparedness.database
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
@@ -10,8 +11,12 @@ import com.example.android.politicalpreparedness.network.models.Election
 interface ElectionDao {
 
     //TODO: Add insert query
+    @Insert
+    fun insert(election: Election)
 
     //TODO: Add select all election query
+    @Query("SELECT * FROM election_table")
+    fun getAll(): LiveData<List<Election>>
 
     //TODO: Add select single election query
 
