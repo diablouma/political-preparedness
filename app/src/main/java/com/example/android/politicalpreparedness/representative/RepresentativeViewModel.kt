a52985b9e4126eedde14e0280ecca6cdb7848a97
diablouma <mh732@hotmail.com>
2021-09-09 00:44:24 +0200

    initalizes spinner with the array of states and attach selected value to live data

--- app/src/main/java/com/example/android/politicalpreparedness/representative/RepresentativeViewModel.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/representative/RepresentativeViewModel.kt
@@ -1,10 +1,27 @@
 package com.example.android.politicalpreparedness.representative
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class RepresentativeViewModel: ViewModel() {
+class RepresentativeViewModel : ViewModel() {
 
     //TODO: Establish live data for representatives and address
+    val _addressLine1 = MutableLiveData<String>()
+    val addressLine1: LiveData<String>
+        get() = _addressLine1
+
+    val _addressLine2 = MutableLiveData<String>()
+    val addressLine2: LiveData<String>
+        get() = _addressLine2
+
+    val _city = MutableLiveData<String>()
+    val city: LiveData<String>
+        get() = _city
+
+    val _state = MutableLiveData<String>()
+    val state: LiveData<String>
+        get() = _state
 
     //TODO: Create function to fetch representatives from API from a provided address
 
